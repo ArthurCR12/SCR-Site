ff945d3c7403037399138e1305dc8b3a772a9b90
Arthur Cabral <134966248+ArthurCR12@users.noreply.github.com>
2024-04-05 17:30:38 -0300

    Update

--- home.js
+++ home.js
@@ -2,9 +2,10 @@
 
 document.getElementById('home').addEventListener('click', () => {
     document.getElementById('a-home').classList.remove('hidden')
-    document.getElementById('a-contato').classList.add('hidden')
+    
 })
 document.getElementById('contato').addEventListener('click', () => {
     document.getElementById('a-home').classList.add('hidden')
     document.getElementById('a-contato').classList.remove('hidden')
 })
+
